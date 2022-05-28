d472e041155741851d1387b1df4089bdd1ea7d12
Rodrigo Grande <rodrigogrande1@hotmail.com>
2022-05-27 20:52:40 -0600

    UnitTest Aplicado

--- payroll_api.sql
+++ payroll_api.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-05-2022 a las 17:45:23
+-- Tiempo de generación: 27-05-2022 a las 18:34:24
 -- Versión del servidor: 10.4.16-MariaDB
 -- Versión de PHP: 7.4.12
 
@@ -42,10 +42,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `name`, `lastName`, `address`, `phoneNumber`, `userName`, `password`) VALUES
-(2, 'prueba', 'segunda', 'diree', 78965234, 'prueb', '123'),
-(3, 'Rebeca', 'Ruano', 'Soyapango', 75236970, 'rebequita', '1234'),
-(5, 'prueba', '2ert', 'Soyapango', 752895, 'prueba', '1234456'),
-(6, 'prueba2', '2ert2', 'Soyapango2', 752812, 'prueba22', '1234456');
+(1, 'Carlos', 'Menjivar', 'San Salvador', 75604578, 'carlos', '12345'),
+(2, 'Maria Elena', 'Sandoval', 'Santa Ana', 78657904, 'mary', '12345');
 
 -- --------------------------------------------------------
 
@@ -60,16 +58,18 @@ CREATE TABLE `boss` (
   `address` varchar(100) NOT NULL,
   `phoneNumber` int(8) NOT NULL,
   `userName` varchar(10) NOT NULL,
-  `password` varchar(8) NOT NULL
+  `password` varchar(8) NOT NULL,
+  `idStatus` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `boss`
 --
 
-INSERT INTO `boss` (`id`, `name`, `lastName`, `address`, `phoneNumber`, `userName`, `password`) VALUES
-(1, 'Luis', 'Panameño', 'Antiguo Cuscatlan', 78956324, 'luisito', '123456'),
-(2, 'Angel', 'Aguilar', 'San Salvador', 75894623, 'angel1', '123456');
+INSERT INTO `boss` (`id`, `name`, `lastName`, `address`, `phoneNumber`, `userName`, `password`, `idStatus`) VALUES
+(1, 'Abraham', 'Guerra', 'San Miguel', 75896342, 'abraham', 'guerra', 1),
+(2, 'Ernesto', 'Navarrete', 'San Miguel', 76507854, 'neto', '123', 1),
+(3, 'gjxgj', 'dzjzj', 'jgjj', 7896324, 'fddh', '1234', 2);
 
 -- --------------------------------------------------------
 
@@ -81,17 +81,18 @@ CREATE TABLE `company` (
   `id` int(11) NOT NULL,
   `name` varchar(60) NOT NULL,
   `address` varchar(100) NOT NULL,
-  `idBoss` int(11) NOT NULL
+  `idBoss` int(11) NOT NULL,
+  `idStatus` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `company`
 --
 
-INSERT INTO `company` (`id`, `name`, `address`, `idBoss`) VALUES
-(2, 'comunication2', 'San Salvador', 1),
-(3, 'comunication3', 'San Salvador', 1),
-(4, 'comunication4', 'San Miguel', 1);
+INSERT INTO `company` (`id`, `name`, `address`, `idBoss`, `idStatus`) VALUES
+(1, 'kodigo', 'Chalatenango', 1, 1),
+(2, 'tecnoGroup', 'San Salvador', 2, 1),
+(3, 'TecnoGroup3', 'Panama', 2, 2);
 
 -- --------------------------------------------------------
 
@@ -105,20 +106,70 @@ CREATE TABLE `employee` (
   `lastName` varchar(60) NOT NULL,
   `phoneNumber` int(8) NOT NULL,
   `address` varchar(100) NOT NULL,
+  `position` varchar(100) NOT NULL,
   `salary` float NOT NULL,
   `userName` varchar(10) NOT NULL,
   `password` varchar(8) NOT NULL,
   `idBoss` int(11) NOT NULL,
-  `idCompany` int(11) NOT NULL
+  `idCompany` int(11) NOT NULL,
+  `idStatus` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `employee`
 --
 
-INSERT INTO `employee` (`id`, `name`, `lastName`, `phoneNumber`, `address`, `salary`, `userName`, `password`, `idBoss`, `idCompany`) VALUES
-(1, 'Jose', 'Landaverde', 75896323, 'Santa Ana', 500, 'jose', '123', 1, 0),
-(2, 'Maria', 'Solar', 75896587, 'Santa Tecla', 450, 'marias', '12345', 1, 0);
+INSERT INTO `employee` (`id`, `name`, `lastName`, `phoneNumber`, `address`, `position`, `salary`, `userName`, `password`, `idBoss`, `idCompany`, `idStatus`) VALUES
+(1, 'Jose', 'Aguilar', 7569876, 'Aguilares', 'Sala de ventas', 750, 'jose', '1234', 1, 1, 1),
+(2, 'Angelica', 'Reyes', 78964523, 'Chalatenango', 'Contadora', 800, 'angelica', '1234', 1, 1, 1),
+(3, 'fEggg', 'efgg', 12345, 'sffsaf', 'Contadora', 800, 'afd', '1234', 1, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `status`
+--
+
+INSERT INTO `status` (`id`, `name`) VALUES
+(1, 'active'),
+(2, 'inactive');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `test`
+--
+
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `test`
+--
+
+INSERT INTO `test` (`id`, `name`) VALUES
+(1, 'rodrigo'),
+(2, 'rebeca'),
+(3, 'neto'),
+(4, 'maria'),
+(5, 'maria'),
+(6, 'maria'),
+(7, 'maria'),
+(8, 'otro'),
+(9, 'otro'),
+(10, 'otro'),
+(11, 'otro');
 
 --
 -- Índices para tablas volcadas
@@ -134,14 +185,16 @@ ALTER TABLE `admin`
 -- Indices de la tabla `boss`
 --
 ALTER TABLE `boss`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_idStatus` (`idStatus`);
 
 --
 -- Indices de la tabla `company`
 --
 ALTER TABLE `company`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_idBoss` (`idBoss`);
+  ADD KEY `fk_idBoss` (`idBoss`),
+  ADD KEY `fk_idStatus` (`idStatus`) USING BTREE;
 
 --
 -- Indices de la tabla `employee`
@@ -149,7 +202,20 @@ ALTER TABLE `company`
 ALTER TABLE `employee`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_idBoss` (`idBoss`),
-  ADD KEY `fk_idCompany` (`idCompany`);
+  ADD KEY `fk_idCompany` (`idCompany`),
+  ADD KEY `fk_idStatus` (`idStatus`);
+
+--
+-- Indices de la tabla `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -159,25 +225,37 @@ ALTER TABLE `employee`
 -- AUTO_INCREMENT de la tabla `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `boss`
 --
 ALTER TABLE `boss`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `company`
 --
 ALTER TABLE `company`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `employee`
 --
 ALTER TABLE `employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `status`
+--
+ALTER TABLE `status`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
